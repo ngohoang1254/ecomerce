452ec0b8fa39cd102db4dab8b69919442d23b72a
hoangngo <ngohoang38212341@gmail.com>
2025-04-21 17:25:29 +0700

    update code list items

--- src/app/[slug]/component/ProductItemList.tsx
+++ src/app/[slug]/component/ProductItemList.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 export default function ProductItemList({ name, price, imageUrl }: IProduct) {
     return (
         <div className="bg-white flex gap-4 border h-full cursor-pointer group hover:shadow-2xl">
-            <Image src={imageUrl} alt="" width={300} height={400}
+            <Image src={imageUrl} alt="" width={200} height={400}
                 className="h-full"
             />
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col">
                 <div className="text-base cursor-pointer py-2">
-                    <div className="text-black group-hover:text-[#0660bb] text-2xl font-bold">{name}</div>
+                    <div className="text-black group-hover:text-[#0660bb] text-lg font-bold truncate max-w-[150px] md:max-w-full">{name}</div>
                     <div className="text-blue font-bold text-[#0660bb]">{price}</div>
                 </div>
                 <div className="flex gap-2 mt-4">
